0605136e955df547b0fcb5802e475c532e651bd4
ArtemKlym <artemklymenko892@gmail.com>
2022-10-06 18:27:59 +0300

    Create the logic for 1 option: "manageContacts()"

--- src/challenge1/Main.java
+++ src/challenge1/Main.java
@@ -31,6 +31,116 @@ public class Main {
             case 2:
                 manageMessages();
                 break;
+            default:
+                break;
         }
     }
+
+    private static void manageContacts() {
+        System.out.println("Select one:"+
+                "\n\t1. Show all contacts"+
+                "\n\t2. Add a new contact"+
+                "\n\t3. Search for a contact"+
+                "\n\t4. Delete a contact"+
+                "\n\t5. Go back");
+
+        int choice = scanner.nextInt();
+        switch(choice){
+            case 1:
+                showAllContacts();
+                break;
+            case 2:
+                addNewContact();
+                break;
+            case 3:
+                searchForContact();
+                break;
+            case 4:
+                deleteContact();
+                break;
+            default:
+                showInitialOptions();
+                break;
+        }
+    }
+
+    private static void deleteContact() {
+        System.out.println("Enter the contact's name:");
+        String name = scanner.next();
+
+        if(name.equals("")){
+            System.out.println("Please enter the name");
+            deleteContact();
+        }
+        else{
+            boolean doesExits = false;
+
+            for(Contacts con: contacts){
+                 if(con.getName().equals(name)){
+                     doesExits = true;
+                     contacts.remove(con);
+                 }
+            }
+
+            if(!doesExits) {
+                System.out.println("There is no such contact");
+            }
+        }
+        showInitialOptions();
+    }
+
+    private static void searchForContact() {
+        System.out.println("Enter the contact name");
+        String name = scanner.next();
+        if(name.equals("")){
+            System.out.println("Please enter the name");
+            searchForContact();
+        }
+        else{
+            boolean doesExist = false;
+            for(Contacts con:contacts){
+                if(con.getName().equals(name)){
+                    doesExist = true;
+                    con.getDetails();
+                }
+            }
+
+            if(!doesExist){
+                System.out.println("There is no such contact in your phone");
+            }
+        }
+
+        showInitialOptions();
+    }
+
+    private static void addNewContact() {
+        System.out.println("Adding a new contact..."+
+                "\nEnter the contact's name");
+        String name = scanner.next();
+        System.out.println("Enter the contact's number");
+        String number = scanner.next();
+        System.out.println("Enter the contact's email");
+        String email = scanner.next();
+
+        if(name.equals("")||number.equals("")||email.equals("")){
+            System.out.println("Please enter all of the information");
+            addNewContact();
+        }
+        else{
+            Contacts contact = new Contacts(name,number,email);
+            contacts.add(contact);
+        }
+
+        showInitialOptions();
+    }
+
+    private static void showAllContacts() {
+        for(Contacts con : contacts){
+            con.getDetails();
+        }
+
+        showInitialOptions();
+    }
+
+
 }
